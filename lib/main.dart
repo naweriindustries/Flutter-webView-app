42a59d92e16fa2d6b1985c3573dcf4e70cf553c1
Naweri Industries <130235251+naweriyowericlinton@users.noreply.github.com>
2024-11-22 12:59:33 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,6 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: WebViewApp(),
-    );
-  }
-}
 
 class WebViewApp extends StatefulWidget {
   @override
