da764e2e578f16ec4ea1e3b9fbdf38b596b96a6e
Naweri Industries <130235251+naweriyowericlinton@users.noreply.github.com>
2024-11-22 13:09:11 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,22 @@ void main() {
   runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    OneSignal.shared.setAppId("5e423775-67c6-42de-8a63-d2e6bbfa7219");  // Set your OneSignal app ID
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: WebViewApp(initialUrl: 'https://flutter.dev'), // Provide an initial URL
+    );
+  }
+}
 
 class WebViewApp extends StatefulWidget {
+  final String initialUrl;  // Correctly declared as a class member variable
+
+  WebViewApp({required this.initialUrl});  // Constructor to initialize the initialUrl
+
   @override
   State<WebViewApp> createState() => _WebViewAppState();
 }
@@ -22,30 +36,12 @@ class _WebViewAppState extends State<WebViewApp> {
         title: const Text('Flutter WebView App'),
       ),
       body: WebView(
-       final String initialUrl;
-        WebViewApp({required this.initialUrl});
-
+        initialUrl: widget.initialUrl,  // Use the initialUrl passed into the widget
         javascriptMode: JavascriptMode.unrestricted,
         onWebViewCreated: (controller) {
           _controller = controller;
         },
       ),
-    );
-  }
-}
-
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    OneSignal.shared.setAppId("5e423775-67c6-42de-8a63-d2e6bbfa7219");  // Set your OneSignal app ID
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(),
     );
   }
 }
